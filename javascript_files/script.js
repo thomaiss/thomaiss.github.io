8af5ae9c2a9616dfa1878a2f0008635734ac5ffb
thomais <tellisjohnson00@gmail.com>
2026-07-08 12:30:12 +0100

    forgot to add new files... website overhaul, including responsive design for compatibility with smaller screens

---
+++ javascript_files/script.js
@@ -0,0 +1,9 @@
+// write your code here, (ofcourse remove the alert as well)
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
